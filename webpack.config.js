4c36e8654c7ab89e25ee91dad86921c7199c9dfd
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-05-21 13:56:54 +0400

    get rid of css and style loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,17 @@
 module.exports = {
-	entry: `${__dirname}/lib/index.jsx`,
+	entry: [
+		`${__dirname}/lib/index.jsx`
+	],
 	output: {
 		path: `${__dirname}/public`,
 		filename: 'bundle.js'
 	},
+	resolve: {
+    extensions: [' ', '.js', '.jsx']
+  },
 	module: {
     rules: [
-      {test: /\.(js|jsx)$/, use: 'babel-loader'},
-			{test: /\.css$/,
-        use: [
-          { loader: 'style-loader'},
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
-          }
-        ]
-			}
+      {test: /\.(js|jsx)$/, use: 'babel-loader'}
     ]
   },
 }
